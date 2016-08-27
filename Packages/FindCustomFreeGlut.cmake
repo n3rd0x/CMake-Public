e9f58736f82ca1456bf2a41160dbb39957f6719a
T.Sang Tran <mail.nerdox@gmail.com>
2016-08-27 19:39:20 +0200

    Update GLEW and FreeGlut includes directory issues.

--- Packages/FindCustomFreeGlut.cmake
+++ Packages/FindCustomFreeGlut.cmake
@@ -22,41 +22,49 @@
 
 # ************************************************************
 # Start package
-message_header( FREEGLUT )
-package_begin( FREEGLUT )
-package_create_home_path( FREEGLUT FREEGLUT_ROOT )
+message_header(FREEGLUT)
+package_begin(FREEGLUT)
+package_create_home_path(FREEGLUT FREEGLUT_ROOT)
+
+
 
 
 # ************************************************************
 # Create search path
-set( FREEGLUT_PREFIX_PATH ${FREEGLUT_HOME} )
-package_create_search_path_include( FREEGLUT )
-package_create_search_path_library( FREEGLUT )
+set(FREEGLUT_PREFIX_PATH ${FREEGLUT_HOME})
+package_create_search_path_include(FREEGLUT)
+package_create_search_path_library(FREEGLUT)
+
+
 
 
 # ************************************************************
 # Options
-option( FREEGLUT_ENABLE_STATIC "Flag for using statical library." OFF )
+option(FREEGLUT_ENABLE_STATIC "Flag for using statical library." OFF)
+
+
 
 
 # ************************************************************
 # Create search name
-set( FREEGLUT_LIBRARY_NAMES "freeglut" )
-if( FREEGLUT_ENABLE_STATIC )
-    package_create_statical_names( FREEGLUT_LIBRARY_NAMES )
+set(FREEGLUT_LIBRARY_NAMES "freeglut") 
+if(FREEGLUT_ENABLE_STATIC)
+    package_create_statical_names(FREEGLUT_LIBRARY_NAMES)
 endif()
-package_create_debug_names( FREEGLUT_LIBRARY_NAMES )
+package_create_debug_names(FREEGLUT_LIBRARY_NAMES)
+
+
 
 
 # ************************************************************
 # Clear
-set( FREEGLUT_CLEAR_IF_CHANGED 
+set(FREEGLUT_CLEAR_IF_CHANGED 
     FREEGLUT_PREFIX_PATH
     FREEGLUT_ENABLE_STATIC
 )
-foreach( VAR ${FREEGLUT_CLEAR_IF_CHANGED} )
-    if( WIN32 AND NOT FREEGLUT_ENABLE_STATIC )
-        package_clear_if_changed( ${VAR}
+foreach(VAR ${FREEGLUT_CLEAR_IF_CHANGED})
+    if(WIN32 AND NOT FREEGLUT_ENABLE_STATIC)
+        package_clear_if_changed(${VAR}
             FREEGLUT_LIBRARY_DEBUG
             FREEGLUT_LIBRARY_RELEASE
             FREEGLUT_PATH_INCLUDE
@@ -64,46 +72,53 @@ foreach( VAR ${FREEGLUT_CLEAR_IF_CHANGED} )
             FREEGLUT_BINARY_RELEASE
         )
     else()
-        package_clear_if_changed( ${VAR}
+        package_clear_if_changed(${VAR}
             FREEGLUT_LIBRARY_DEBUG
             FREEGLUT_LIBRARY_RELEASE
             FREEGLUT_PATH_INCLUDE
         )
-        unset( FREEGLUT_BINARY_DEBUG CACHE )
-        unset( FREEGLUT_BINARY_RELEASE CACHE )
+        unset(FREEGLUT_BINARY_DEBUG CACHE)
+        unset(FREEGLUT_BINARY_RELEASE CACHE)
     endif()
 endforeach()
+unset(VAR)
+
+
 
 
 # ************************************************************
 # Find paths
-package_find_path( FREEGLUT_PATH_INCLUDE "freeglut.h" "${FREEGLUT_SEARCH_PATH_INCLUDE}" "GL" )
-package_find_library( FREEGLUT_LIBRARY_DEBUG "${FREEGLUT_LIBRARY_NAMES_DEBUG}" "${FREEGLUT_SEARCH_PATH_LIBRARY}" "debug" )
-package_find_library( FREEGLUT_LIBRARY_RELEASE "${FREEGLUT_LIBRARY_NAMES}" "${FREEGLUT_SEARCH_PATH_LIBRARY}" "release;relwithdebinfo;minsizerel" )
-package_make_library( FREEGLUT_LIBRARY FREEGLUT_LIBRARY_DEBUG FREEGLUT_LIBRARY_RELEASE )
+package_find_path(FREEGLUT_PATH_INCLUDE "freeglut.h" "${FREEGLUT_SEARCH_PATH_INCLUDE}" "GL")
+package_find_library(FREEGLUT_LIBRARY_DEBUG "${FREEGLUT_LIBRARY_NAMES_DEBUG}" "${FREEGLUT_SEARCH_PATH_LIBRARY}" "debug")
+package_find_library(FREEGLUT_LIBRARY_RELEASE "${FREEGLUT_LIBRARY_NAMES}" "${FREEGLUT_SEARCH_PATH_LIBRARY}" "release;relwithdebinfo;minsizerel")
+package_make_library(FREEGLUT_LIBRARY FREEGLUT_LIBRARY_DEBUG FREEGLUT_LIBRARY_RELEASE)
+
+
 
 
 # ************************************************************
 # Find binaries on Windows
-if( WIN32 AND NOT FREEGLUT_ENABLE_STATIC )
-	set( FREEGLUT_BINARY_NAMES "freeglut" )
-	package_create_debug_binary_names( FREEGLUT_BINARY_NAMES )
-    package_create_release_binary_names( FREEGLUT_BINARY_NAMES )
-	package_create_search_path_binary( FREEGLUT )
+if(WIN32 AND NOT FREEGLUT_ENABLE_STATIC)
+	set(FREEGLUT_BINARY_NAMES "freeglut")
+	package_create_debug_binary_names(FREEGLUT_BINARY_NAMES)
+    package_create_release_binary_names(FREEGLUT_BINARY_NAMES)
+	package_create_search_path_binary(FREEGLUT)
 	
-	set( FREEGLUT_SEARCH_BINARIES 
+	set(FREEGLUT_SEARCH_BINARIES 
 		${FREEGLUT_SEARCH_PATH_BINARY}
 		${FREEGLUT_SEARCH_PATH_LIBRARY}
 	)
 
-	package_find_file( FREEGLUT_BINARY_DEBUG "${FREEGLUT_BINARY_NAMES_DEBUG}" "${FREEGLUT_SEARCH_BINARIES}" "debug" )
-	package_find_file( FREEGLUT_BINARY_RELEASE "${FREEGLUT_BINARY_NAMES_RELEASE}" "${FREEGLUT_SEARCH_BINARIES}" "release;relwithdebinfo;minsizerel" )
+	package_find_file(FREEGLUT_BINARY_DEBUG "${FREEGLUT_BINARY_NAMES_DEBUG}" "${FREEGLUT_SEARCH_BINARIES}" "debug")
+	package_find_file(FREEGLUT_BINARY_RELEASE "${FREEGLUT_BINARY_NAMES_RELEASE}" "${FREEGLUT_SEARCH_BINARIES}" "release;relwithdebinfo;minsizerel")
 endif()
+
+
 
 
 # ************************************************************
 # Finalize package
-package_validate( FREEGLUT )
-package_add_parent_dir( FREEGLUT )
-package_end( FREEGLUT )
-message_footer( FREEGLUT )
+package_validate(FREEGLUT)
+package_add_parent_dir(FREEGLUT ADD_PARENT)
+package_end(FREEGLUT)
+message_footer(FREEGLUT)
--- Packages/FindCustomGLEW.cmake
+++ Packages/FindCustomGLEW.cmake
@@ -22,65 +22,69 @@
 
 # ************************************************************
 # Start package
-message_header( GLEW )
-package_begin( GLEW )
-package_create_home_path( GLEW GLEW_ROOT )
+message_header(GLEW)
+package_begin(GLEW)
+package_create_home_path(GLEW GLEW_ROOT)
+
+
 
 
 # ************************************************************
 # x64-bit system
 # Check whether we are going to compile for x64-bit systems.
-string( REGEX MATCH "Win64" x64_FOUND ${CMAKE_GENERATOR} )
+string(REGEX MATCH "Win64" x64_FOUND ${CMAKE_GENERATOR})
 # Set path suffix for x64.
-if( x64_FOUND )
-    set( PATH_SUFFIX "x64" )
+if(x64_FOUND )
+    set(PATH_SUFFIX "x64")
 else()
-    set( PATH_SUFFIX "x86" )
+    set(PATH_SUFFIX "x86")
 endif()
-unset( x64_FOUND )
+unset(x64_FOUND)
+
+
 
 
 # ************************************************************
 # Create search path
-set( GLEW_PREFIX_PATH ${GLEW_HOME} )
-package_create_search_path_include( GLEW )
-package_create_search_path_library( GLEW )
+set(GLEW_PREFIX_PATH ${GLEW_HOME})
+package_create_search_path_include(GLEW)
+package_create_search_path_library(GLEW)
 
 
 # ************************************************************
 # Options
-option( GLEW_ENABLE_MX "Flag for using the MX (Multiple Rendering Contexts)." OFF )
-option( GLEW_ENABLE_STATICAL "Flag for using static library." OFF )
+option(GLEW_ENABLE_MX "Flag for using the MX (Multiple Rendering Contexts)." OFF)
+option(GLEW_ENABLE_STATICAL "Flag for using static library." OFF)
 
 
 # ************************************************************
 # Create search name
-if( GLEW_ENABLE_MX )
-    if( GLEW_ENABLE_STATICAL )
-        set( GLEW_LIBRARY_NAMES "glew32mx" "glewmx" )
+if(GLEW_ENABLE_MX)
+    if(GLEW_ENABLE_STATICAL)
+        set(GLEW_LIBRARY_NAMES "glew32mx" "glewmx")
     else()
-        set( GLEW_LIBRARY_NAMES "glew32mxs" "glewmxs" )
+        set(GLEW_LIBRARY_NAMES "glew32mxs" "glewmxs")
     endif()
 else()
-    if( GLEW_ENABLE_STATICAL )
-        set( GLEW_LIBRARY_NAMES "glew32s" "glews" )
+    if(GLEW_ENABLE_STATICAL)
+        set(GLEW_LIBRARY_NAMES "glew32s" "glews")
     else()
-        set( GLEW_LIBRARY_NAMES "glew32" "glew" )
+        set(GLEW_LIBRARY_NAMES "glew32" "glew")
     endif()
 endif()
-package_create_debug_names( GLEW_LIBRARY_NAMES )
+package_create_debug_names(GLEW_LIBRARY_NAMES)
 
 
 # ************************************************************
 # Clear
-set( GLEW_CLEAR_IF_CHANGED 
+set(GLEW_CLEAR_IF_CHANGED 
     GLEW_ENABLE_MX
     GLEW_ENABLE_STATICAL
     GLEW_PREFIX_PATH
 )
-foreach( VAR ${GLEW_CLEAR_IF_CHANGED} )
-    if( WIN32 )
-        package_clear_if_changed( ${VAR}
+foreach(VAR ${GLEW_CLEAR_IF_CHANGED})
+    if(WIN32)
+        package_clear_if_changed(${VAR}
             GLEW_LIBRARY_DEBUG
             GLEW_LIBRARY_RELEASE
             GLEW_PATH_INCLUDE
@@ -88,50 +92,55 @@ foreach( VAR ${GLEW_CLEAR_IF_CHANGED} )
             GLEW_BINARY_DEBUG
         )
     else()
-        package_clear_if_changed( ${VAR}
+        package_clear_if_changed(${VAR}
             GLEW_LIBRARY_DEBUG
             GLEW_LIBRARY_RELEASE
             GLEW_PATH_INCLUDE
         )
     endif()
 endforeach()
+unset(VAR)
+
+
 
 
 # ************************************************************
 # Find paths
-package_find_path( GLEW_PATH_INCLUDE "glew.h" "${GLEW_SEARCH_PATH_INCLUDE}" "GL" )
-package_find_library( GLEW_LIBRARY_DEBUG "${GLEW_LIBRARY_NAMES_DEBUG}" "${GLEW_SEARCH_PATH_LIBRARY}" "${PATH_SUFFIX}"  )
-package_find_library( GLEW_LIBRARY_RELEASE "${GLEW_LIBRARY_NAMES}" "${GLEW_SEARCH_PATH_LIBRARY}" "${PATH_SUFFIX}"  )
-package_make_library( GLEW_LIBRARY GLEW_LIBRARY_DEBUG GLEW_LIBRARY_RELEASE )
+package_find_path(GLEW_PATH_INCLUDE "glew.h" "${GLEW_SEARCH_PATH_INCLUDE}" "GL")
+package_find_library(GLEW_LIBRARY_DEBUG "${GLEW_LIBRARY_NAMES_DEBUG}" "${GLEW_SEARCH_PATH_LIBRARY}" "${PATH_SUFFIX}")
+package_find_library(GLEW_LIBRARY_RELEASE "${GLEW_LIBRARY_NAMES}" "${GLEW_SEARCH_PATH_LIBRARY}" "${PATH_SUFFIX}")
+package_make_library(GLEW_LIBRARY GLEW_LIBRARY_DEBUG GLEW_LIBRARY_RELEASE)
+
+
 
 
 # ************************************************************
 # Find binaries on Windows
-if( WIN32 AND NOT GLEW_ENABLE_STATICAL )
-    if( GLEW_ENABLE_MX )
-        set( GLEW_BINARY_NAMES "glew32mx" )
+if(WIN32 AND NOT GLEW_ENABLE_STATICAL)
+    if(GLEW_ENABLE_MX )
+        set(GLEW_BINARY_NAMES "glew32mx")
     else()
-        set( GLEW_BINARY_NAMES "glew32" )
+        set(GLEW_BINARY_NAMES "glew32")
     endif()
     
-    package_create_debug_binary_names( GLEW_BINARY_NAMES )
-	package_create_release_binary_names( GLEW_BINARY_NAMES )
-	package_create_search_path_binary( GLEW )
+    package_create_debug_binary_names(GLEW_BINARY_NAMES)
+	package_create_release_binary_names(GLEW_BINARY_NAMES)
+	package_create_search_path_binary(GLEW)
 	
-	set( GLEW_SEARCH_BINARIES 
+	set(GLEW_SEARCH_BINARIES 
 		${GLEW_SEARCH_PATH_BINARY}
 		${GLEW_SEARCH_PATH_LIBRARY}
 	)
 	
-	package_find_file( GLEW_BINARY_DEBUG "${GLEW_BINARY_NAMES_DEBUG}" "${GLEW_SEARCH_BINARIES}" "${PATH_SUFFIX}" )
-	package_find_file( GLEW_BINARY_RELEASE "${GLEW_BINARY_NAMES_RELEASE}" "${GLEW_SEARCH_BINARIES}" "${PATH_SUFFIX}" )
+	package_find_file(GLEW_BINARY_DEBUG "${GLEW_BINARY_NAMES_DEBUG}" "${GLEW_SEARCH_BINARIES}" "${PATH_SUFFIX}")
+	package_find_file(GLEW_BINARY_RELEASE "${GLEW_BINARY_NAMES_RELEASE}" "${GLEW_SEARCH_BINARIES}" "${PATH_SUFFIX}")
 endif()
 
 
 # ************************************************************
 # Finalize package
-package_validate( GLEW )
-package_add_parent_dir( GLEW )
-package_end( GLEW )
-message_footer( GLEW )
+package_validate(GLEW)
+package_add_parent_dir(GLEW ADD_PARENT)
+package_end(GLEW)
+message_footer(GLEW)
 
