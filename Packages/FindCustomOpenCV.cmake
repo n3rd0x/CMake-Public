c0cf21cebb4f68e01d26f0b8642d6896d8f819b8
T.Sang Tran <nerdox.tranit@gmail.com>
2019-12-08 15:41:22 +0100

    Support OpenCV version greater than 4.

--- Packages/FindCustomOpenCV.cmake
+++ Packages/FindCustomOpenCV.cmake
@@ -5,10 +5,10 @@
 # the rights to use, copy, modify, merge, publish, distribute, sublicense,
 # and/or sell copies of the Software, and to permit persons to whom the
 # Software is furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
 # OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -22,18 +22,23 @@
 
 # ************************************************************
 # Start package
+# ************************************************************
 message_header(OPENCV)
 package_begin(OPENCV)
 package_create_home_path(OPENCV OPENCV_ROOT)
 
 # Options.
+option(OPENCV_LEGACY "Searching for legacy version. Version less than 4.0.0" ON)
 if(MSVC)
     set(OPENCV_VERSION "340" CACHE STRING "OpenCV version to search for.")
 endif()
 
 
+
+
 # ************************************************************
 # Create search path
+# ************************************************************
 if(MSVC)
     string(REGEX MATCH "Win64" x64Found ${CMAKE_GENERATOR})
     set(MsvcVer "vc12")
@@ -53,6 +58,7 @@ package_create_search_path_library(OPENCV)
 
 # ************************************************************
 # Create search name
+# ************************************************************
 if(MSVC)
     set(OPENCV_LIBRARY_NAMES "opencv_world${OPENCV_VERSION}")
 else()
@@ -65,6 +71,7 @@ package_create_debug_names(OPENCV_LIBRARY_NAMES)
 
 # ************************************************************
 # Clear
+# ************************************************************
 package_clear_if_changed(OPENCV_HOME
     OPENCV_PATH_INCLUDE
     OPENCV_LIBRARY_DEBUG
@@ -80,7 +87,12 @@ package_clear_if_changed(OPENCV_HOME
 
 # ************************************************************
 # Find paths
-package_find_path(OPENCV_PATH_INCLUDE "cv.h" "${OPENCV_SEARCH_PATH_INCLUDE}" "opencv")
+# ************************************************************
+if(OPENCV_LEGACY)
+    package_find_path(OPENCV_PATH_INCLUDE "cv.h" "${OPENCV_SEARCH_PATH_INCLUDE}" "opencv")
+else()
+    package_find_path(OPENCV_PATH_INCLUDE "opencv2" "${OPENCV_SEARCH_PATH_INCLUDE}" "opencv4")
+endif()
 package_find_library(OPENCV_LIBRARY_DEBUG "${OPENCV_LIBRARY_NAMES_DEBUG}" "${OPENCV_SEARCH_PATH_LIBRARY}" "")
 package_find_library(OPENCV_LIBRARY_RELEASE "${OPENCV_LIBRARY_NAMES}" "${OPENCV_SEARCH_PATH_LIBRARY}" "")
 package_make_library(OPENCV_LIBRARY OPENCV_LIBRARY_DEBUG OPENCV_LIBRARY_RELEASE)
@@ -94,17 +106,17 @@ if(WIN32)
     set(OPENCV_BINARY_NAMES ${OPENCV_LIBRARY_NAMES})
 	package_create_release_binary_names(OPENCV_BINARY_NAMES)
 	package_create_debug_binary_names(OPENCV_BINARY_NAMES)
-    
+
     set(OPENCV_BINARY_FFMPEG_NAMES "opencv_ffmpeg${OPENCV_VERSION}")
     package_create_release_binary_names(OPENCV_BINARY_FFMPEG_NAMES)
 	package_create_debug_binary_names(OPENCV_BINARY_FFMPEG_NAMES)
-    
+
 	package_create_search_path_binary(OPENCV)
-	set(OPENCV_SEARCH_BINARIES 
+	set(OPENCV_SEARCH_BINARIES
 		${OPENCV_SEARCH_PATH_BINARY}
 		${OPENCV_SEARCH_PATH_LIBRARY}
 	)
-    
+
     package_find_file(OPENCV_BINARY_DEBUG "${OPENCV_BINARY_NAMES_DEBUG}" "${OPENCV_SEARCH_BINARIES}" "")
 	package_find_file(OPENCV_BINARY_RELEASE "${OPENCV_BINARY_NAMES_RELEASE}" "${OPENCV_SEARCH_BINARIES}" "")
     package_find_file(OPENCV_BINARY_FFMPEG_DEBUG "${OPENCV_BINARY_FFMPEG_NAMES_DEBUG}" "${OPENCV_SEARCH_BINARIES}" "")
@@ -114,7 +126,7 @@ if(WIN32)
             list(APPEND OPENCV_BINARY_DEBUG "${OPENCV_BINARY_FFMPEG_DEBUG}")
         endif()
     endif()
-    
+
     if(OPENCV_BINARY_RELEASE)
         if(OPENCV_BINARY_FFMPEG_RELEASE)
             list(APPEND OPENCV_BINARY_RELEASE "${OPENCV_BINARY_FFMPEG_RELEASE}")
@@ -127,6 +139,7 @@ endif()
 
 # ************************************************************
 # Finalize package
+# ************************************************************
 package_validate(OPENCV)
 package_add_parent_dir(OPENCV ADD_PARENT)
 package_end(OPENCV)
